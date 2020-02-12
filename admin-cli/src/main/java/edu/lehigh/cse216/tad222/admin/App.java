1f4dd4e87163a697f6837ccd9a35d4f7ded5c2e1
Tal Derei <talderei@dyn230242.cc.lehigh.edu>
2020-02-12 17:32:03 -0500

    added the skeleton of an admin app, using default maven archetype

---
+++ admin-cli/src/main/java/edu/lehigh/cse216/tad222/admin/App.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse216.tad222.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
