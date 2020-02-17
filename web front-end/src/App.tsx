1447d27b098b843f85d9dd7f6bc3c9621135a7c7
Anmol Shrestha <ans221@lehigh.edu>
2020-02-17 00:58:04 -0500

    add google login and heroku dependencies to the app

--- web front-end/src/App.tsx
+++ web front-end/src/App.tsx
@@ -1,44 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 const App = () => {
-  const [toDisplay, setToDisplay] = useState<String>('Edit src/App.tsx and save to reload.');
-  useEffect(() => {
-    const response = fetch('http://localhost:5000/', {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-    }).then((response) => {
-      return response.text();
-    })
-      .then((myResponse) => {
-        setToDisplay(myResponse);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-  function handleClick(e: any) {
-    e.preventDefault();
-    setToDisplay('Wow Congratulations you did some stuff');
+
+  const [signedIn, setSignedIn] = useState<boolean>(false);
+  const [userName, setUserName] = useState<string>('Patient0');
+
+  const responseGoogle = (response: any) => {
+    // console.log(response);
+    const user = response.Qt.Ad;
+    setUserName(user);
+    response.Ca ? setSignedIn(true) : setSignedIn(false);
+    const appLogo = document.getElementById("App-logo");
+    const loginButton = document.getElementById("login-button");
+    loginButton!.style.display = "none";
+    if (appLogo!.style.display === "none") {
+      appLogo!.style.display = "block";
+    } else {
+      appLogo!.style.display = "none";
+    }
   }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {toDisplay}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={handleClick}>Click Me for some Magic</button>
+        <img src={logo} className="App-logo" alt="logo" id="App-logo" />
+        <div id="login-button">
+          <GoogleLogin
+            className="googleLoginButton"
+            clientId="131496045117-i2iui8ssoajm8dlcvhenkcf64h4ieb0k.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        </div>
       </header>
     </div>
   );
