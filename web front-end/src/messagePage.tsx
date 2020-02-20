fd5c9e137f32ebfa4dbb524405174c9baf52ac86
Anmol Shrestha <ans221@lehigh.edu>
2020-02-20 13:29:09 -0500

    add message input box

--- web front-end/src/messagePage.tsx
+++ web front-end/src/messagePage.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleLogout } from 'react-google-login';
 
 
 const MessagePage = (props: any): JSX.Element | null => {
+  const [enteredMessage, setEnteredMessage] = useState<string>(`Hi, What's up?`);
+
+  function handleMessageInputChange() {
+    setEnteredMessage(enteredMessage);
+  }
   if (props.signedIn)
     return <p id="messages">
       {props.userName} <br />
+      <form>
+        <label>
+          Enter the message:
+    <input type="text" name="name" value={enteredMessage} onChange={handleMessageInputChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       <GoogleLogout
         className="googleLogoutButton"
         clientId="372884561524-22jfggk3pefbnanh83o92mqqlmkbvvd9.apps.googleusercontent.com"
