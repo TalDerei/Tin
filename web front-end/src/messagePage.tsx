de4085c5f6b0c817e1bb702fb7dc3d1e17f244cd
Anmol Shrestha <ans221@lehigh.edu>
2020-02-20 12:44:50 -0500

    put in a logout page

--- web front-end/src/messagePage.tsx
+++ web front-end/src/messagePage.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+
 
 const MessagePage = (props: any): JSX.Element => {
   if (props.signedIn)
-    return <p>Here I am</p>;
+    return <p>
+      Here I am <br/>
+      <GoogleLogout
+        className="googleLogoutButton"
+        clientId="372884561524-22jfggk3pefbnanh83o92mqqlmkbvvd9.apps.googleusercontent.com"
+        buttonText="Logout"
+      >
+      </GoogleLogout>
+    </p>;
   else
     return <p>xyz</p>;
 }
