dfbdd1a5c48837e07e43830b3f0592c9061398bd
Glycanstructure <sap716@lehigh.edu>
2020-03-31 06:02:46 -0400

    ProfileActivity to show profile page

---
+++ android/app/src/main/java/edu/lehigh/cse216/teamtin/ProfileActivity.java
@@ -0,0 +1,55 @@
+package edu.lehigh.cse216.teamtin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ProfileActivity extends AppCompatActivity {
+    public static final String GOOGLE_ACCOUNT = "google_account";
+    private TextView profileName, profileEmail;
+    private ImageView profileImage;
+    private Button signOut;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        String messageUser;
+        messageUser = intent.getStringExtra("messageUser");
+        ArrayList<String> messages = intent.getStringArrayListExtra("messages");
+
+        Log.d("messageUser", messageUser);
+        Log.d("#messages", "size: " + messages.size());
+
+        RecyclerView rv = findViewById(R.id.datum_list_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        ItemListInProfileAdapter adapter = new ItemListInProfileAdapter(this, messages);
+        rv.setAdapter(adapter);
+    }
+}
